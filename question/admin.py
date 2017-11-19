5d12f1a704eeeccd5b2cf1d228e57bcc7e9e6288
joerow <joerow@Josephs-MacBook.local>
2017-11-19 21:04:54 +0800

    PhysicsQuiz

---
+++ question/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import CalculationQuestion
+
+# Register your models here.
+admin.site.register(CalculationQuestion)
---
+++ question/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class CalculationQuestion(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+    topic = models.CharField(max_length=200)
+    answer = models.DecimalField
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
