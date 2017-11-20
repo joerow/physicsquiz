0cf81359a5dfecfc1c296dabb369ca880e477da5
joerow <joerow@Josephs-MacBook.local>
2017-11-20 21:22:36 +0800

    Modified templated to display CalculationQuestions from the database

--- question/views.py
+++ question/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import CalculationQuestion
+from django.utils import timezone
 # Create your views here.
 
 
 def question_list(request):
-    return render(request, 'question/question_list.html')
+    questions = CalculationQuestion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'question/question_list.html',{'questions': questions})
