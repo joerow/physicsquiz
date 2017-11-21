638aff2832d59fcacdef3a5464d55bcdc0f6f060
joerow <joerow@Josephs-MacBook.local>
2017-11-21 18:39:14 +0800

    Updated CSS to visual studio

--- question/views.py
+++ question/views.py
@@ -6,4 +6,4 @@ from django.utils import timezone
 
 def question_list(request):
     questions = CalculationQuestion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'question/question_list.html',{'questions': questions})
+    return render(request, 'question/question_list.html', {'questions': questions})
